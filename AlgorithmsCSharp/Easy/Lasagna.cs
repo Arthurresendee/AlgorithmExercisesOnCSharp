4fea6c430dc1904b835c24327d13ecf9ffd7e40b
ArthurWork <Arthurresende2004@gmail.com>
2023-10-07 21:17:21 -0300

    First Challenge - Lasagna

---
+++ AlgorithmsCSharp/Easy/Lasagna.cs
@@ -0,0 +1,13 @@
+﻿
+public class Lasagna {
+    public int expectedMinutesInOven = 40;
+
+    public int ExpectedMinutesInOven() => 40;
+
+    public int RemainingMinutesInOven(int currentTime) =>  ExpectedMinutesInOven() - currentTime;
+
+    public int PreparationTimeInMinutes(int numberOfLayers) => numberOfLayers *= 2;
+
+    public int ElapsedTimeInMinutes(int numberOfLayers, int xpectedMinutesInOven) =>
+        PreparationTimeInMinutes( numberOfLayers) + xpectedMinutesInOven;
+}
