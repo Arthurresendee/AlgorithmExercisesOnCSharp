a38852970178a99ef380859feac9300db963d6cd
ArthurWork <Arthurresende2004@gmail.com>
2023-10-12 09:35:27 -0300

    Finalizando primeiro exercício - básico

--- AlgorithmsCSharp/Easy/Program.cs
+++ AlgorithmsCSharp/Easy/Program.cs
@@ -1,16 +1,1 @@
 ﻿
-var lasagna = new Lasagna();
-Console.WriteLine(lasagna.ExpectedMinutesInOven());
-// => 40
-
-var lasagna2 = new Lasagna();
-Console.WriteLine(lasagna2.RemainingMinutesInOven(30));
-// => 10
-
-var lasagna3 = new Lasagna();
-Console.WriteLine(lasagna3.PreparationTimeInMinutes(2));
-// => 4
-
-var lasagna4 = new Lasagna();
-Console.WriteLine(lasagna4.ElapsedTimeInMinutes(3, 20));
-// => 26
