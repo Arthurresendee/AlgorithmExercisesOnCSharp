d49d64493153df88cd53592871c6ee04ff12a604
ArthurWork <Arthurresende2004@gmail.com>
2023-11-08 16:23:01 -0300

    fazerndo mais dois exercísios simples

---
+++ AlgorithmsCSharp/Easy/BMI.cs
@@ -0,0 +1,30 @@
+﻿using AlgorithmsCSharp.Easy;
+
+namespace AlgorithmsCSharp.Easy
+{
+    public class BMI
+    {
+        public static string Bmi(double weight, double height)
+        {
+            if(weight > 0 && height > 0)
+            {
+                double result = weight / Math.Pow(height, 2);
+
+                return result <= 18.5 ? "Underweight" :
+                result <= 25.0 ?"Normal" :
+                result <= 30.0 ? "Overweight" :
+                "Obese";
+
+                //if (result <= 18.5) return "Underweight";
+                //else if (result <= 25.0) return "Normal";
+                //else if (result <= 30.0) return "Overweight";
+                //return "Obese";
+            }
+            return "any parameter cannot be equal to 0 or less";      
+        }
+    }
+}
+
+//using AlgorithmsCSharp.Easy;
+
+//Console.WriteLine(BMI.Bmi(58, 1.84));
---
+++ AlgorithmsCSharp/Easy/Name.cs
@@ -0,0 +1,22 @@
+﻿
+using AlgorithmsCSharp.Easy;
+
+namespace AlgorithmsCSharp.Easy
+{
+    public static class Name
+    {
+        public static string Greet(string name)
+        {
+            Console.WriteLine(name);
+            return !string.IsNullOrEmpty(name) ? $"Hello, {name} how are you doing today?" : "name is empty";
+        }
+    }
+
+}
+
+
+//using AlgorithmsCSharp.Easy;
+
+//Console.Write("Escreva seu nome: ");
+//string name = Console.ReadLine();
+//Name.Greet(name);
